c0baf549b7e7448281fcea954d989832217186da
OussamaTabDev <oussamatabzioui09@gmail.com>
2025-06-07 09:18:16 +0100

    learing Some

--- lib/Models/Weather.dart
+++ lib/Models/Weather.dart
@@ -1,21 +1,21 @@
 class Weather {
-  final String CityName;
-  final double Temperature;
-  final String MainCondition;
+  final String cityName;
+  final double temperature;
+  final String mainCondition;
 
   Weather({
-    required this.CityName,
-    required this.Temperature,
-    required this.MainCondition,
+    required this.cityName,
+    required this.temperature,
+    required this.mainCondition,
   });
 
   factory Weather.fromJson(
     Map<String , dynamic> json
   ){
       return Weather(
-        CityName: json['name'] ,
-        Temperature: json['main']['temp'].toDouble() ,
-        MainCondition: json['weather'][0]['main'] ,
+        cityName: json['name'] ,
+        temperature: json['main']['temp'].toDouble() ,
+        mainCondition: json['weather'][0]['main'] ,
         );
   } 
 }
