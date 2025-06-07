a966d0187d477074f909489541b80e340e4f1963
OussamaTabDev <oussamatabzioui09@gmail.com>
2025-06-07 09:13:14 +0100

    learing Some

---
+++ lib/Pages/Weather_Page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatelessWidget {
+
+  const WeatherPage({ super.key });
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('hellow rodl'),
+      ),
+      body: Container(),
+    );
+  }
+}
